3226e67550d47282f9270dc315d3dfe155330453
kareem fawas <phawazzzy@gmail.com>
2020-06-23 03:21:20 +0100

    test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ const app = require("../index");
 
     describe("GET /", () => {
       it("respond with Hello World", (done) => {
-        request(app).get("/").expect("Hello World", done);
+        request(app).get("/").expect("Hello World this is fawas", done);
       })
     });
