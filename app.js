aec3d5c7b64e1b6a7c1449f9c986ae16f09444f8
kareem fawas <phawazzzy@gmail.com>
2020-06-23 21:36:17 +0100

    body parsing

--- app.js
+++ app.js
@@ -11,13 +11,21 @@
 
 
  app.use(express.json());
+ app.use(express.urlencoded());
 
  app.use(logger('dev'));
  app.use(cookieParser());
  
 
- // Respond with "hello world" for requests that hit our root "/"
+ // Respond with welcome... for requests that hit our root "/"
  app.get("/", (req, res) =>  res.send("<h2>Welcome to NigeriaApi</h2><p><a href=\"/api/v1\">Check out version 1 of the Nigeria API</a></p>"));
+
+ //test for body
+ app.get("/api/v1/", (req, res) => {
+    const { data } = req.body;
+    console.log(data);
+    res.send('okay');
+ });
 
 
  module.exports = app;
