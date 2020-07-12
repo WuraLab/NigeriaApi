0efc31bb213e1757a7337b69ec0f812ca31e3d7b
kareem fawas <phawazzzy@gmail.com>
2020-07-12 21:19:43 +0100

    ft send mail to succesfully registered user

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ## University API
 
 # App Details
+### how to get started
+to run this app locally, you should have node js installed
+run git clone repo link
+cd into the directory
+run npm install
+
 .......
 ................................................
 ................................
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,4 +1,6 @@
 const db = require("../models/index");
+const mailer = require("../helpers/mailer");
+const { generateMailForSignup } = require("./email/helper");
 
 const Users = db.users;
 const { hashPassword, isPasswordValid, generateToken } = require("../helpers/authHelper");
@@ -6,10 +8,12 @@ const { hashPassword, isPasswordValid, generateToken } = require("../helpers/aut
 exports.signup = async (req, res) => {
   try {
     // get values from body
+    // the password should be atleast, contain atleast one number,
+    //  it only accept this symbols !@#$%^&*;
     const {
       username, email, password, confirmPassword
     } = req.body;
-    // checks if password and confirmPasword matche
+    // checks if password and confirmPasword match
     if (password !== confirmPassword) {
       return res.status(403).json({ response: "confirm password not matched with password" });
     }
@@ -17,12 +21,25 @@ exports.signup = async (req, res) => {
     // hash password
     const hash = await hashPassword(confirmPassword);
     const token = await generateToken({ email });
-    console.log(token);
+    // console.log(token);
     // create userdetails to Database
-    const created = await Users.create({
+    const createdUser = await Users.create({
       email, username, password: hash
     });
-    return res.status(201).json({ response: "SignUp successful, check your email for validation", created });
+
+    const validationLink = `http://localhost:3000/api/v1/validate/${token}`;
+
+    // generateMailForSignup is  a function that returns an html file
+    const options = {
+      receiver: email,
+      subject: "NigeriaApi E-mail validation",
+      text: "validate your account",
+      output: generateMailForSignup(validationLink, email)
+    };
+
+    // function to send the mail
+    await mailer(options);
+    return res.status(201).json({ response: "SignUp successful, check your email for validation", createdUser });
   } catch (error) {
     return res.status(500).json({ response: error.message });
   }
