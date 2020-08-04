2d79699cb76a1d5efdc9994de9eb7c552b17ace4
kareem fawas <phawazzzy@gmail.com>
2020-08-04 20:10:16 +0100

    fixes cors errors

--- api/app.js
+++ api/app.js
@@ -15,7 +15,16 @@ dotenv.config();
 const indexRouter = require("./routes/index.js");
 
 const app = express();
-
+// this is to prevent CORS errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next();
+})
 
 app.use(logger("dev"));
 app.use(express.json());
