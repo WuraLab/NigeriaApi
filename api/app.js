6741e346721217680d4024eebc609ab5d60b5e36
kareem fawas <phawazzzy@gmail.com>
2020-08-06 19:22:14 +0100

    fixes issues of absence of  production database url

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ const indexRouter = require("./routes/index.js");
 const app = express();
 // this is to prevent CORS errors
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
+res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
--- api/config/config.js
+++ api/config/config.js
@@ -8,7 +8,8 @@ const {
   LOCAL_DATABASE_USER,
   LOCAL_DATABASE_PASSWORD,
   LOCAL_DATABASE_HOST,
-  LOCAL_DATABASE_PORT
+  LOCAL_DATABASE_PORT,
+  DEV_DB_URI
 } = process.env;
 
 // DEV_DATABASE_NAME= 'db74jvlfa32vit'
@@ -19,11 +20,36 @@ const {
 
 module.exports = {
   development: {
-    username: "trxbthfngzrayf",
-    password: "23054ed29fc3e310ae2000c0be157d980dda0a95af17e5c18a936a46f8c4fb1d",
-    database: 'db74jvlfa32vit',
-    host: "ec2-50-16-198-4.compute-1.amazonaws.com",
-    port: 5432,
+    username: DEV_DATABASE_USER,
+    password: DEV_DATABASE_PASSWORD,
+    database: DEV_DATABASE_NAME,
+    host: DEV_DATABASE_HOST,
+    port: DEV_DATABASE_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      keepAlive: true
+    },
+    ssl: true,
+    // logging: false
+  },
+  test: {
+    username: LOCAL_DATABASE_USER,
+    password: LOCAL_DATABASE_PASSWORD,
+    database: LOCAL_DATABASE_NAME,
+    host: LOCAL_DATABASE_HOST,
+    port: LOCAL_DATABASE_PORT,
+    dialect: "postgres"
+  },
+  production: {
+    username: DEV_DATABASE_USER,
+    password: DEV_DATABASE_PASSWORD,
+    database: DEV_DATABASE_NAME,
+    host: DEV_DATABASE_HOST,
+    port: DEV_DATABASE_PORT,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -34,20 +60,5 @@ module.exports = {
     },
     ssl: true,
     logging: false
-  },
-  test: {
-    username: LOCAL_DATABASE_USER,
-    password: LOCAL_DATABASE_PASSWORD,
-    database: LOCAL_DATABASE_NAME,
-    host: LOCAL_DATABASE_HOST,
-    port: LOCAL_DATABASE_PORT,
-    dialect: "postgres"
   }
-  // "production": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_production",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // }
 };
