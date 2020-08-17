f34c5522a6aeee317da036b32127369a2c9f3f54
kareem fawas <phawazzzy@gmail.com>
2020-08-17 23:10:34 +0100

    added response for errors

--- api/controllers/university.js
+++ api/controllers/university.js
@@ -5,25 +5,29 @@ const { university_data } = db;
 const query = require("../helpers/query");
 
 exports.allUniversity = async (req, res) => {
-  const { Type, dateRange, limit } = req.query;
+  const { limit } = req.query;
   if (!req.user || req.user === undefined) {
     return res.status(403).json({ response: "you dont have access to this endpoint" });
   }
   const dbQuery = query(req.query);
-  console.log("dbQuery", dbQuery)
 
-  let response;
-  // check for all the query parameter here and run each one by one
-  if (req.query) {
-    response = await university_data.findAll({
-      limit,
-      attributes: {
-        exclude: ["createdAt", "updatedAt"]
-      },
-      where: dbQuery
-    });
-  } else {
-    response = await university_data.findAll({limit, attributes: { exclude: ["createdAt", "updatedAt"] } });
+  try {
+    let response;
+    // check for all the query parameter here and run each one by one
+    if (req.query) {
+      response = await university_data.findAll({
+        limit,
+        attributes: {
+          exclude: ["createdAt", "updatedAt"]
+        },
+        where: dbQuery
+      });
+    } else {
+      response = await university_data.findAll({limit, attributes: { exclude: ["createdAt", "updatedAt"] } });
+    }
+    return res.status(200).json({ length: response.length, response: response });
+  } catch (error) {
+    return res.status(500).json({ response: `internal server error ${error}` })
   }
-  return res.status(200).json({ length: response.length, response: response });
+ 
 };
--- api/helpers/query.js
+++ api/helpers/query.js
@@ -1,12 +1,17 @@
 const { Op } = require("sequelize")
 
 let query = (query) => {
+  try {
     let Query = {};
     const { Type, dateRange, region } = query;
     if (Type != null) Query.Type = Type
     if (dateRange != null) Query.Founded = { [Op.gte]: dateRange }
-    if (region != null || region != undefined) Query.Region = region 
-    return Query ;
+    if (region != null || region != undefined) Query.Region = region
+    return Query;
+  } catch (error) {
+    return res.status(500).json({ response: `error ${error} from the query middleware` })
   }
-  
-  module.exports = query;
+ 
+}
+
+module.exports = query;
