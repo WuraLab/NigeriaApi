d4425c5a3ae8387021775e30d99f8f46994e2c7b
kareem fawas <phawazzzy@gmail.com>
2020-10-02 18:36:43 +0100

    feature get data of one polytechnic using name as a params formatted code to look good to the eyes😃😃😃

--- api/controllers/university.js
+++ api/controllers/university.js
@@ -24,38 +24,40 @@ exports.allUniversity = async (req, res) => {
         where: dbQuery
       });
     } else {
-      response = await university_data.findAll({limit, attributes: { exclude: ["createdAt", "updatedAt"] } });
+      response = await university_data.findAll({ limit, attributes: { exclude: ["createdAt", "updatedAt"] } });
     }
     return res.status(200).json({ length: response.length, response: response });
   } catch (error) {
     return res.status(500).json({ response: `internal server error ${error}` })
 
   }
- 
+
 };
 
 
 exports.oneUniversity = async (req, res) => {
-  const name  = req.params.name;
+  const name = req.params.name;
   const capitalize = name.toUpperCase();
   console.log(capitalize);
-try {
-  
-  if (!req.user || req.user === undefined) {
-    return res.status(401).json({ response: "you dont have access to this endpoint" });
-  }
+  try {
 
-  const response = await university_data.findOne({ attributes: { exclude: ["createdAt", "updatedAt"] }, 
-  where: {
-    Abbrevation: capitalize
-  }})
-  if(response == null || response.length < 1) {
-    return res.status(404).json({ response: "data not found, probably we dont have the requested university data" })
-  } 
-  return res.status(200).json({ response });
-} catch (error) {
-  return res.status(500).json({ response: `${error} occured`})
-}
+    if (!req.user || req.user === undefined) {
+      return res.status(401).json({ response: "you dont have access to this endpoint" });
+    }
+
+    const response = await university_data.findOne({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+      where: {
+        Abbrevation: capitalize
+      }
+    })
+    if (response == null || response.length < 1) {
+      return res.status(404).json({ response: "data not found, probably we dont have the requested university data" })
+    }
+    return res.status(200).json({ response });
+  } catch (error) {
+    return res.status(500).json({ response: `${error} occured` })
+  }
 
 
 }
