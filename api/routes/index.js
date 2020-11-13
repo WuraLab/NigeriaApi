20ca272cc982223db0b4f95c7a6cb3588458599c
oluwaseun <dadsclement@gmail.com>
2020-11-13 03:49:19 +0100

    Documented the route files

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,20 +9,45 @@ const { userLoginSchema, userSchema } = require("../helpers/validationSchema");
 const { allPolytechnic, onePolytechnic } = require("../controllers/polytechnic");
 
 // USERS RELATED ENDPOINTS
+/**
+ * This is the users authentication routes
+ * the route allow users to signup, login and generate API Key
+ */
 router.post("/users/", validator(userSchema), signup);
 router.get("/validate/users", validate);
 router.post("/users/login", validator(userLoginSchema), login);
 router.get("/users/generateApikey", checkAuth, generateApikey);
 
+
 // UNIVERSIRY RELATED ENDPOINT
+/**
+ * this is the universities data route
+ * users can get all universities and a single university data by name.
+ * the route uses a middleware to validate the users
+ * and enable the user to generate API keys
+ *
+ */
 router.get("/university", validateUserToken, allUniversity);
 router.get("/university/:name", validateUserToken, oneUniversity);
 
 // POLYTECHNIC RELATED ENDPOINT
+
+/**
+ * this is the polytechnic data route
+ * users can get all polytechnic and a single polytechnic data by name
+ * the route uses a middleware to validate the users
+ * and enable the user to generate API keys
+ *
+ */
 router.get("/polytechnic", validateUserToken, allPolytechnic);
 router.get("/polytechnic/:name", validateUserToken, onePolytechnic);
 
 // ADMINISTARTIVE ROUTES
+/**
+ * This routes is for the admin
+ * it enables the admin to perform
+ * crud operations and update the api data.
+ */
 router.route("/universities/:id")
     .all(checkAuthAdmin)
     .delete(deleteUniversity)
